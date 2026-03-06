08eef019e87ac69c474b6899ef77541c341ae041
Harsh Mehta <147304413+HarzhMehta@users.noreply.github.com>
2026-03-06 16:21:08 +0530

    update nextjs version and add game + event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -185,6 +185,7 @@ export default function Home() {
                     <p className="font-bold">₹150</p>
                   </div>
                 </div>
+                
                 <a
                   href="https://learner.vierp.in/"
                   target="_blank"
